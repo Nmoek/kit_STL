a816c2a81043dc13d84250786c0cfe062b94580d
Nmoek <707204150@qq.com>
2023-05-14 04:25:10 +0800

    [ADD/CHG]初步编写CMakeLists.txt;增加git忽略项;开始构思空间配置器代码alloctor.h

---
+++ kit_STL/alloctor.h
@@ -0,0 +1,15 @@
+#ifndef __KIT_ALLOCTOR_H__
+#define __KIT_ALLOCTOR_H__
+
+#include <cstdlib>
+
+
+namespace kit_STL {
+
+
+
+
+}
+
+
+#endif
---
+++ tests/test_alloctor.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file test_alloctor.cpp
+ * @author Kewin Li
+ * @brief  测试配置器(Alloctor)
+ * @version 0.1
+ * @date 2023-05-14
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <iostream>
+
+
+
+int main()
+{
+
+    std::cout << "66666666666666" << std::endl;
+
+    return 0;
+}
