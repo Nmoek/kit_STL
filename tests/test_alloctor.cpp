e15a2a6ec4ba22fc800dcfa6fe61620045fb7ada
Nmoek <707204150@qq.com>
2023-06-13 23:28:51 +0800

    [CHG] 修改编译参数;对标准库allocator、内存池进行测试

--- tests/test_alloctor.cpp
+++ tests/test_alloctor.cpp
@@ -9,13 +9,152 @@
  *
  */
 #include <iostream>
+#include <sys/time.h>
+#include <vector>
+#include <ext/pool_allocator.h>
+
+#include "kit_STL/kit_allocator.h"
+
+#define TIME_2_DIFF(t1, t2)     ((t2.tv_sec - t1.tv_sec)*1000 + (t2.tv_usec - t1.tv_usec)/1000)
 
 
+/**
+ * @brief 测试kit_STL库的分配器
+ */
+#include <vector>
+void test_kit_alloctor()
+{
+    std::vector<int, kit_STL::kit_allocator<int> > m_v;
 
-int main()
+    for(int i = 0;i < 10;++i)
+    {
+        m_v.push_back(i + 1);
+    }
+
+    for(int i = 0;i < 10;++i)
+    {
+        m_v.front();
+        m_v.pop_back();
+    }
+}
+
+
+/**
+ * @brief 测试内存池
+ *
+ */
+void test_pool_allocator()
+{
+    struct timeval t1 = {0};
+    struct timeval t2 = {0};
+
+#if 0
+    std::vector<int> m_v;
+
+    gettimeofday(&t1, NULL);
+    for(int i = 0;i < 10000000;++i)
+    {
+        m_v.push_back(i + 1);
+    }
+    gettimeofday(&t2, NULL);
+
+    printf("cost time=%lld ms \n", TIME_2_DIFF(t1, t2));
+
+#else
+    std::vector<int, __gnu_cxx::__pool_alloc<int> > m_v;
+    gettimeofday(&t1, NULL);
+    for(int i = 0;i < 10000000;++i)
+    {
+        m_v.push_back(i + 1);
+    }
+    gettimeofday(&t2, NULL);
+
+    printf("cost time=%lld ms \n", TIME_2_DIFF(t1, t2));
+
+#endif
+}
+
+
+/**
+ * @brief 测试STL标准库的分配器
+ */
+void test_stl_allocator()
 {
 
-    std::cout << "66666666666666" << std::endl;
+    auto m_allocator = std::allocator<int>();
+    struct timeval t1 = {0};
+    struct timeval t2 = {0};
+
+#if 1
+
+    gettimeofday(&t1, NULL);
+    for(int i = 0;i < 100000;++i)
+    {
+        auto p = m_allocator.allocate(10*1024*1024, (int *)0);
+        m_allocator.deallocate(p, 10*1024*1024);
+    }
+    gettimeofday(&t2, NULL);
+
+    printf("cost time=%lld ms \n", TIME_2_DIFF(t1, t2));
+
+    return;
+
+#else
+    gettimeofday(&t1, NULL);
+    for(int i = 0;i < 100000;++i)
+    {
+        auto p = new int[10*1024*1024];
+        delete p;
+    }
+    gettimeofday(&t2, NULL);
+
+    printf("cost time=%lld ms \n", TIME_2_DIFF(t1, t2));
+
+    return;
+#endif
+}
+
+
+
+int main(int argc, char *argv[])
+{
+
+    if(argc < 2)
+    {
+        printf("input: xxx [num] \n");
+        return 0;
+    }
+
+
+    switch(argv[1][0])
+    {
+        case '1':
+        {
+            test_stl_allocator();
+            break;
+        }
+
+        case '2':
+        {
+            test_pool_allocator();
+            break;
+        }
+
+        case '3':
+        {
+            test_kit_alloctor();
+            break;
+        }
+
+        default:
+            printf("type %c err! \n", argv[1][0]);
+            return 0;
+
+    }
+
+
+
+
 
     return 0;
 }
